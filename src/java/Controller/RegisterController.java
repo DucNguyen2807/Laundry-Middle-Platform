2f8a1faaaaac2f7f6ef4815b8b72fa1729877739
Nguyen Xuan Duc <140423571+ducnguyen2807@users.noreply.github.com>
2023-10-12 16:40:44 +0700

    chinh lai thu vien

--- src/java/Controller/RegisterController.java
+++ src/java/Controller/RegisterController.java
@@ -11,11 +11,11 @@ import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
