cd6e82ce7da014718ec8a71908689cae13ff5826
nguye <nguyevitien2003@gmail.com>
2023-11-02 15:17:14 +0700

    them view order cus

---
+++ src/java/Controller/ViewOrderCustomerController.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import Model.Order;
+import Model.User;
+import Service.OrderService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author nguye
+ */
+
+public class ViewOrderCustomerController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            String button = request.getParameter("btAction");
+            
+            User user = (User) request.getSession().getAttribute("user");
+            int userId = user.getUserId();
+            OrderService ord = new OrderService();
+            try {
+                if (button.equals("NewOrderCus")) {
+                    ord.viewOrderCustomer(userId, "1");
+                } else if (button.equals("ProcessingCustomer")) {
+                    ord.viewOrderCustomer(userId, "4");
+                } else if (button.equals("CompletedCustomer")) {
+                    ord.viewOrderCustomer(userId, "5");
+                }
+                
+                List<Order> result = ord.getListOrder();
+                request.setAttribute("SEARCHRESULT", result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                RequestDispatcher rd = request.getRequestDispatcher("ordercustomer.jsp");
+                    rd.forward(request, response);
+            }
+        }
+        }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
